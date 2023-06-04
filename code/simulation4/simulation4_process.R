7d533cc80bca5669dfcc7c6098911c3e538cb390
Soumik Purkayastha <soumikp@umich.edu>
2023-06-04 13:45:38 -0400

    jmva revisions

--- code/simulation4/simulation4_process.R
+++ code/simulation4/simulation4_process.R
@@ -16,57 +16,7 @@ p_load(
   readxl, here, rdrop2, lubridate, zoo, tidyverse, purrr, data.table, stringr, tidyr, vroom
 )
 
-
-#source("/home/soumikp/2022_jmva/code/functions.R")
-#source("/home/soumikp/2022_jmva/code/simulation1/simulation1_patterns.R")
-
-dir <- file.path("/home/soumikp/2022_jmva/output", "simulation4")
-files <- list.files(dir, pattern = ".csv") 
-
-
-mse <- function(estim, true){
-  mean((estim - true)**2)
-}
-
-results <- NULL
-
-
-results = matrix(file.path(dir, files) %>% map_dfr(., read.csv) %>% pull(2), byrow = T, ncol = 18)
-colnames(results) = c("sampsize", "rho", "iter", 
-                      "ex.emi", "ex.jmi", "ex.fmi", 
-                      "ar1.emi", "ar1.jmi", "ar1.fmi", 
-                      "spatial.emi", "spatial.jmi", "spatial.fmi", 
-                      "mild.emi", "mild.jmi", "mild.fmi", 
-                      "strong.emi", "strong.jmi", "strong.fmi")
-                      
-results <- as.tibble(results) %>% 
-  group_by(sampsize, rho) %>%
-  summarize(pow.ex.fmi = mean(ex.fmi <= 0.05),
-            pow.ex.jmi = mean(ex.jmi <= 0.05),
-            pow.ex.emi = mean(ex.emi <= 0.05),
-            
-            pow.ar1.fmi = mean(ar1.fmi <= 0.05),
-            pow.ar1.jmi = mean(ar1.jmi <= 0.05),
-            pow.ar1.emi = mean(ar1.emi <= 0.05),
-            
-            pow.spatial.fmi = mean(spatial.fmi <= 0.05),
-            pow.spatial.jmi = mean(spatial.jmi <= 0.05),
-            pow.spatial.emi = mean(spatial.emi <= 0.05),
-            
-            pow.mild.fmi = mean(mild.fmi <= 0.05),
-            pow.mild.jmi = mean(mild.jmi <= 0.05),
-            pow.mild.emi = mean(mild.emi <= 0.05),
-            
-            pow.strong.fmi = mean(strong.fmi <= 0.05),
-            pow.strong.jmi = mean(strong.jmi <= 0.05),
-            pow.strong.emi = mean(strong.emi <= 0.05),
-            .groups = "drop") %>% 
-  pivot_longer(cols = -c(sampsize, rho)) %>% 
-  rowwise() %>% 
-  mutate(family = str_to_title(unlist(strsplit(name, "[.]"))[2]), 
-         type = unlist(strsplit(name, "[.]"))[3])
-  
-  
+results = read_csv(file.path(here(), "simulation4.csv"))  
 
 plot_title = "Comparison of empirical power of competing tests of independences based on mutual information (MI) for different configurations of various copula families for varying sample sizes."
 plot_subtitle = "The dotted black line parallel to the x-axis denotes specified level of significance = 0.05."
@@ -118,7 +68,7 @@ p <- results %>%
                      ) 
 
 
-ggsave(file.path("/home/soumikp/2022_jmva/tlfs", "simulation4_figure.pdf"), 
+ggsave(file.path(here(), "simulation4_figure.pdf"), 
        p,
        width = 8.5,
        height = 11,
